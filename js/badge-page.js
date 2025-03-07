4a0f2dfd349474f87a9229fc9c9fa97ae97d1390
vadik184 <133372327+vadik184@users.noreply.github.com>
2025-03-07 16:57:25 +0200

    v2.0

---
+++ js/badge-page.js
@@ -0,0 +1,5 @@
+const maleRadio = document.getElementById("#male");
+const femaleRadio = document.getElementById("#female");
+const nameInput = document.getElementById("#name");
+
+console.log(nameInput, maleRadio, femaleRadio);
